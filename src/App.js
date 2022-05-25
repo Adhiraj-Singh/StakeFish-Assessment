db7f21ecf7bc9b7ac2e8fc72624c0e1a74becff5
Adhiraj Singh <86346441+Adhiraj-Singh@users.noreply.github.com>
2022-05-25 11:38:48 +0530

    Click for info text on cards, fixed flex
    uploaded original source code with comments, non prettied

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Infobox from "./Infobox";
 function App() {
   return (
     <div className="App">
-      
+      {/*calling infobox with 10 cards in main app, alternate would be to make card component, but 10 fixed so no need*/}
       <Infobox/>
 
     </div>
@@ -14,5 +14,6 @@ function App() {
     
   );
 }
+
 
 export default App;
--- src/Infobox.js
+++ src/Infobox.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
+/*axios needed to use api url*/
 import axios from 'axios'
 
 import './Infobox.css'
@@ -8,6 +9,7 @@ const Infobox = () => {
     const [data, setData] = useState(null)
     const url = 'https://api.coingecko.com/api/v3/exchanges?per_page=10';
     
+    /*use url and set data, throw error if it fails*/
     useEffect(()=> {
         axios.get(url).then((response)=> {
             setData(response.data)
@@ -25,21 +27,27 @@ const Infobox = () => {
         <div className = 'Infobox'>
 
             
-
+            {/* The api info needed is for the first 10 cards only, stored in an array [0-9]*/}
             <div className = 'container'>
                 <div className='card'>
+                    {/*logo of exchange*/}
                     <img id = "mylogo" src={data[0].image} alt=''/>
-
+                    {/*name of exchange*/}
                     <h1>{data[0].name}</h1>
 
-                    {/*Highlighting data type for ease of read*/}
+                    {/*Highlighting data type for ease of read using bold*/}
                     
+                    {/*Country based*/}
                     <p><strong>Country</strong> - {data[0].country}</p>
                     
+                    {/*url to exchange website*/}
                     <p><strong>URL</strong> - {data[0].url}</p>
 
+                    {/*Ranking of the exchange*/}
                     <p><strong>Trust Rank</strong> - #{data[0].trust_score_rank}</p>
-                    
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                 </div>
 
                 <div className='card'>
@@ -54,6 +62,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[1].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[1].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -69,6 +80,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[2].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[2].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -84,6 +98,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[3].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[3].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -99,6 +116,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[4].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[4].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -114,6 +134,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[5].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[5].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -129,6 +152,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[6].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[6].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -144,6 +170,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[7].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[7].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
@@ -159,10 +188,14 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[8].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[8].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
                 <div className='card'>
+                    
                     <img id = "mylogo" src={data[9].image} alt=''/>
 
                     <h1>{data[9].name}</h1>
@@ -174,6 +207,9 @@ const Infobox = () => {
                     <p><strong>URL</strong> - {data[9].url}</p>
 
                     <p><strong>Trust Rank</strong> - #{data[9].trust_score_rank}</p>
+
+                    {/*adding this text for call to action*/}
+                    <h2>CLICK FOR MORE INFO</h2>
                     
                 </div>
 
